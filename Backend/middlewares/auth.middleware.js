0f4a111b9d976a3f25ac63d2891c7b68637b7924
rajeshbelgaonkar19 <vu2f2223015@pvppcoe.ac.in>
2025-03-17 23:59:20 +0530

    Captain login, Profile Done!

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next) =>{
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -8,7 +9,7 @@ module.exports.authUser = async (req, res, next) =>{
         return res.status(401).json({ message: 'Unauthorized'});
     }
 
-    const isBlackListed = await userModel.findOne({ token: token });
+    const isBlackListed = await blackListTokenModel.findOne({ token: token });
 
     if (isBlackListed){
         return res.status(401).json({message: 'Unauthorized'});
@@ -23,6 +24,27 @@ module.exports.authUser = async (req, res, next) =>{
 
         return next();
 
+    } catch(err){
+        return res.status(401).json({ message: 'Unauthorized'});
+    }
+}
+
+module.exports.authCaptain = async (req, res, next) =>{
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+    if(!token){
+        return res.status(401).json({ message: 'Unauthorized'});
+    }
+
+    const isBlackListed = await blackListTokenModel.findOne({ token: token });
+
+    if (isBlackListed){
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+    
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await  captainModel.findById(decoded._id);
+        req.user = captain;
     } catch(err){
         return res.status(401).json({ message: 'Unauthorized'});
     }
--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const captainController = require('../controllers/captain.controller');
 const express = require('express');
 const router =express.Router();
 const { body } = require("express-validator");
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -14,4 +15,14 @@ router.post('/register',[
 ],
     captainController.registerCaptain
 )
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6}).withMessage('Password must be at least 6 character long')
+],
+    captainController.loginCaptain
+)
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile)
+
 module.exports =router;
