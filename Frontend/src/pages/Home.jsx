bbb8a0ebf7ce69fbdbe7f95a0d81acf9b4f5de61
rajeshbelgaonkar19 <vu2f2223015@pvppcoe.ac.in>
2025-03-22 00:27:38 +0530

    Home UI

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -6,9 +6,9 @@ const Home  =() =>{
         <div>
             <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'> 
                 <img className='w-16 ml-8' src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png" alt="" />
-                <div className='bg-white'>
-                    <h2>Get Started with Uber</h2>
-                    <button>Continue</button>
+                <div className='bg-white pb-7 py-4 px-4'>
+                    <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+                    <button className='w-full bg-black text-white py-3 rounded mt-5'>Continue</button>
                 </div>
             </div>
         </div>
