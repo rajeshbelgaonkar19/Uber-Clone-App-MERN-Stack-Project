f7fbfb04f5886643bf7aa57ba8759261133808e2
rajeshbelgaonkar19 <vu2f2223015@pvppcoe.ac.in>
2025-03-22 00:00:07 +0530

    Frontend!

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,15 @@ import { Routes } from 'react-router-dom'
 
 const Home  =() =>{
     return(
-        <div>Home</div>
+        <div>
+            <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'> 
+                <img className='w-16 ml-8' src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png" alt="" />
+                <div className='bg-white'>
+                    <h2>Get Started with Uber</h2>
+                    <button>Continue</button>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home
