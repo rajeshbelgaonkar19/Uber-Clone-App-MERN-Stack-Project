f574c9cc086801dd3fac3d532a795cdc3574bc97
rajeshbelgaonkar19 <vu2f2223015@pvppcoe.ac.in>
2025-03-24 23:53:48 +0530

    Home UI update!

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -3,7 +3,16 @@ import { Routes } from 'react-router-dom'
 
 const UserLogin =() =>{
     return(
-        <div>UserLogin</div>
+        <div className='p-7'>
+            <form>
+                <h3 className='text-xl mb-2'>What's Your Email</h3>
+                <input required type="email" placeholder='email@example.com' />
+                <h3>Enter Password</h3>
+                <input required type="password" placeholder='password' />
+                <button>Login</button>
+
+            </form>
+        </div>
     )
 }
 export default UserLogin
