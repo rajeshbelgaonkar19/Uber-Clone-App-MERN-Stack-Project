cee99aa95d12c47641977e24ac8dfdf7b5ddcac7
rajeshbelgaonkar19 <vu2f2223015@pvppcoe.ac.in>
2025-04-01 23:51:03 +0530

    update home!

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home  =() =>{
                 <img className='w-16 ml-8' src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png" alt="" />
                 <div className='bg-white pb-7 py-4 px-4'>
                     <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
-                    <Link to='/login' className=' flex items-center justify center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+                    <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
                 </div>
             </div>
         </div>
